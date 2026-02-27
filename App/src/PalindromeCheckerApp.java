7ff94e272ffb0b5d5ccaf03f4ea4eff594335f1e
Ravish-08 <ravishmugelan@gmail.com>
2026-02-27 11:24:00 +0530

    uc4 - hardcode palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -6,20 +6,36 @@ public class PalindromeCheckerApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter text: ");
+        // Ask user for input
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
+        // Convert the string into a character array
+        char[] chars = input.toCharArray();
+
+        // Initialize pointers
+        int start = 0;
+        int end = chars.length - 1;
+
+        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        // Continue comparison until pointers cross
+        while (start < end) {
+            if (chars[start] != chars[end]) {
                 isPalindrome = false;
                 break;
             }
+            start++;
+            end--;
         }
 
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome: " + isPalindrome);
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+        }
 
         scanner.close();
     }
