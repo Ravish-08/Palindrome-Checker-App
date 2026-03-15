90cf8013566cf741c452f8affc9aa7c98150e8f7
Ravish-08 <ravishmugelan@gmail.com>
2026-03-16 01:27:59 +0530

    UC13 Performance comparison of palindrome algorithms

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 
-// Strategy Interface
-interface PalindromeStrategy {
-    boolean isPalindrome(String str);
-}
+public class PalindromeCheckerApp {
 
-// Strategy 1: Simple Two-Pointer Algorithm
-class SimplePalindromeStrategy implements PalindromeStrategy {
+    // Two Pointer Method
+    public static boolean twoPointerPalindrome(String str) {
 
-    public boolean isPalindrome(String str) {
         str = str.toLowerCase().replaceAll("\\s+", "");
 
         int left = 0;
@@ -24,24 +20,18 @@ class SimplePalindromeStrategy implements PalindromeStrategy {
 
         return true;
     }
-}
 
-// Strategy Context
-class PalindromeContext {
+    // Recursive Method
+    public static boolean recursivePalindrome(String str, int start, int end) {
 
-    private PalindromeStrategy strategy;
+        if (start >= end)
+            return true;
 
-    public PalindromeContext(PalindromeStrategy strategy) {
-        this.strategy = strategy;
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return recursivePalindrome(str, start + 1, end - 1);
     }
-
-    public boolean checkPalindrome(String str) {
-        return strategy.isPalindrome(str);
-    }
-}
-
-// Main Application
-public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
@@ -50,16 +40,23 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Choose algorithm strategy
-        PalindromeStrategy strategy = new SimplePalindromeStrategy();
+        input = input.toLowerCase().replaceAll("\\s+", "");
 
-        PalindromeContext context = new PalindromeContext(strategy);
+        // Two Pointer Performance
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerPalindrome(input);
+        long end1 = System.nanoTime();
 
-        if (context.checkPalindrome(input)) {
-            System.out.println("The string is a Palindrome");
-        } else {
-            System.out.println("The string is NOT a Palindrome");
-        }
+        // Recursive Performance
+        long start2 = System.nanoTime();
+        boolean result2 = recursivePalindrome(input, 0, input.length() - 1);
+        long end2 = System.nanoTime();
+
+        System.out.println("Two Pointer Result: " + result1);
+        System.out.println("Time Taken: " + (end1 - start1) + " ns");
+
+        System.out.println("Recursive Result: " + result2);
+        System.out.println("Time Taken: " + (end2 - start2) + " ns");
 
         sc.close();
     }
