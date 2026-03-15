c367199570477b27ba0d081a94407150ccd05b23
Ravish-08 <ravishmugelan@gmail.com>
2026-03-16 01:12:12 +0530

    UC12 Strategy pattern implementation for palindrome algorithm

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
-// Service class for palindrome checking
-class PalindromeService {
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean isPalindrome(String str);
+}
+
+// Strategy 1: Simple Two-Pointer Algorithm
+class SimplePalindromeStrategy implements PalindromeStrategy {
 
     public boolean isPalindrome(String str) {
-
         str = str.toLowerCase().replaceAll("\\s+", "");
 
         int left = 0;
         int right = str.length() - 1;
 
         while (left < right) {
-
             if (str.charAt(left) != str.charAt(right)) {
                 return false;
             }
-
             left++;
             right--;
         }
@@ -24,17 +26,36 @@ class PalindromeService {
     }
 }
 
+// Strategy Context
+class PalindromeContext {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeContext(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean checkPalindrome(String str) {
+        return strategy.isPalindrome(str);
+    }
+}
+
+// Main Application
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        PalindromeService service = new PalindromeService();
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        if (service.isPalindrome(input)) {
+        // Choose algorithm strategy
+        PalindromeStrategy strategy = new SimplePalindromeStrategy();
+
+        PalindromeContext context = new PalindromeContext(strategy);
+
+        if (context.checkPalindrome(input)) {
             System.out.println("The string is a Palindrome");
         } else {
             System.out.println("The string is NOT a Palindrome");
